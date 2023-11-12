ae284fb29af8e18527ae6acdbe50fa70dcc1975b
Nader Tak <120279049+nadertak7@users.noreply.github.com>
2023-11-12 15:22:38 -0800

    Refactor: make database relational

--- bespokefunctions/db.py
+++ bespokefunctions/db.py
@@ -10,32 +10,48 @@ conn = st.connection('mysql', type='sql')
 SENDER_EMAIL_ID = str(os.environ.get('SENDER_EMAIL_ID'))
 
 def sendToDb(email, subject, message):
-    # Query that is executed when submit button is pressed
-    query = text(   '''
+    # Queries that are executed when submit button is pressed
+    email_sent_log_query = text(   '''
                     INSERT INTO     email_sent_log (email_sender, 
                                                     email_receiver, 
-                                                    email_subject, 
-                                                    email_contents, 
                                                     created_at)
                     VALUES (        :sender_email_id, 
                                     :email, 
-                                    :subject, 
-                                    :message, 
                                     :created_at)
+                                    ''')
+    
+    email_contents_query = text('''
+                    INSERT INTO    email_contents (email_sent_log_id, 
+                                                    email_subject,
+                                                    email_message)
+                        VALUES (    :log_id,
+                                    :subject,
+                                    :message)
                                     ''')
 
     # Values inputted in said query
-    values = {
+    email_sent_log_values = {
         'sender_email_id': SENDER_EMAIL_ID,
         'email': email,
-        'subject': subject,
-        'message': message,
         'created_at': datetime.now()
+    }
+
+    email_contents_values = {
+        'log_id': None,
+        'subject': subject,
+        'message': message
     }
     
     # Streamlit's way of performing queries that write to a database
     with conn.session as session:
-        session.execute(query, values)
+        # Executes email_sent_log insert
+        email_sent_log_execution = session.execute(email_sent_log_query, email_sent_log_values)
+        # Populates email_contents.email_log_id FK 
+        log_id = email_sent_log_execution.lastrowid
+        email_contents_values["log_id"] = log_id
+        # Executes eamil_contents insert 
+        session.execute(email_contents_query, email_contents_values)
+        # Commit
         session.commit()
 
 def readFromDb():
@@ -49,16 +65,17 @@ def readFromDb():
                                     FROM        email_sent_log
                                    ''', ttl = 0)
     avg_char_lengths = conn.query('''
-                                SELECT 		AVG(CHAR_LENGTH(email_contents)) message_char_length,
+                                SELECT 		AVG(CHAR_LENGTH(email_message)) message_char_length,
                                             AVG(CHAR_LENGTH(email_subject))subject_char_length
-                                FROM		email_sent_log 
+                                FROM		email_contents
                                 ''')
     
     # Post-Process Query Results
     send_count = int(send_count.values[0])
     last_message_sent = str(last_message_sent.values[0])[2:-2]
-    avg_subject_length = avg_char_lengths.iloc[0]["subject_char_length"].round(2)
-    avg_message_length = avg_char_lengths.iloc[0]["message_char_length"].round(2)
+    # ([variable] or 0) returns first truthy value
+    avg_subject_length = round((avg_char_lengths.iloc[0]["subject_char_length"] or 0), 2) 
+    avg_message_length = round((avg_char_lengths.iloc[0]["message_char_length"] or 0), 2)
 
     # Pack variables into tuple, to be unpacked in homepage
     return (send_count, 
