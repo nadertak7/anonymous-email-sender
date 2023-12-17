7f4c06f54bfdd096f1ed92ab96a9e8a65688594f
Nader Tak <120279049+nadertak7@users.noreply.github.com>
2023-12-16 17:14:20 -0800

    Chore: remove pycache from git

--- bespokefunctions/filterprofanity.py
+++ bespokefunctions/filterprofanity.py
@@ -5,11 +5,16 @@ def filterProfanity(subject,
                     message, 
                     filter_profanity):
 
+    # Determines whether subject and message contain profanity. Bool will be inerted in db in bespokefuncitons/db.py
     subject_contains_profanity = profanity.contains_profanity(subject)
     message_contains_profanity = profanity.contains_profanity(message)
 
+    # If filter profanity is selected in homepage, censor. 
     if filter_profanity:
         subject = profanity.censor(subject)
-        message = profanity.censor(message)
+        message = profanity.censor(message)    
     
-    return subject, message, subject_contains_profanity, message_contains_profanity
+    return (subject, 
+            message, 
+            subject_contains_profanity, 
+            message_contains_profanity)
